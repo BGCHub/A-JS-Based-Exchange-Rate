2ee965ef7d66898276c2d82b9bada810c2fe987a
K525 <tianze.wen@ucdconnect.ie>
2019-01-26 20:27:36 +0800

    增加国旗&货币标识，实现三种货币转换

--- exchange.js
+++ exchange.js
@@ -1,12 +1,29 @@
+var endpoint = 'live'
+var access_key = '582d231ada529b0aa4ee97477ef8d6ed';
+var USDCNY=0,USDEUR=0;
+
+$.ajax({
+    url: 'http://www.apilayer.net/api/' + endpoint + '?access_key=' + access_key,   
+    dataType: 'jsonp',
+    async: false,
+    success: function(json) {
+        USDCNY=json.quotes.USDCNY;
+        USDEUR=json.quotes.USDEUR;
+    }
+});
+
 window.onload=function(){
+
 	var buttons=document.getElementsByClassName("number");
 	var monitors=document.getElementsByClassName("monitor");
+	var monitorContent=document.getElementsByClassName("monitor-content");
 	var operations=document.getElementsByClassName("operation");
 
 	var isMonitor=new Array(false,false,false);
 	var f="",ff=0;
 	var temp="";
 	var monitorNum=0;
+	var CNY=0,USD=0,EUR=0;
 
 	for(let i=0;i<3;i++){
 
@@ -20,7 +37,7 @@ window.onload=function(){
 			isMonitor[i]=true;
 			monitors[i].style.backgroundColor="#3a4c55";
 			monitors[i].style.fontSize="2rem";
-			monitors[i].style.cssText+="margin: -8px;";
+			monitors[i].style.cssText+="margin: -8px; margin-right: 0;";
 			monitorNum=i;
 			f="";
 		}
@@ -30,28 +47,42 @@ window.onload=function(){
 		buttons[i].onclick=function(){
 			ff=buttons[i].innerHTML;
 			f=f+ff;
-			monitors[monitorNum].innerHTML=f;
+			monitorContent[monitorNum].innerHTML=f;
 		}
 	}
 
 
 	operations[0].onclick=function(){
 		f=f+".";
-		monitors[monitorNum].innerHTML=monitors[monitorNum].innerHTML+".";
+		monitorContent[monitorNum].innerHTML=monitorContent[monitorNum].innerHTML+".";
 	}
 
 	operations[1].onclick=function(){
 		f=f.substring(f.length-1,0);
-		monitors[monitorNum].innerHTML=f;
+		monitorContent[monitorNum].innerHTML=f;
 	}
 
 	operations[2].onclick=function(){
 		f="";
-		monitors[monitorNum].innerHTML=f;
+		for(let i=0;i<3;i++){
+			monitorContent[i].innerHTML=f;
+		}
 	}
 
 	operations[3].onclick=function(){
-
+		if(monitorNum==0){
+			var m0=monitorContent[monitorNum].innerHTML;
+			monitorContent[1].innerHTML=m0/USDCNY;
+			monitorContent[2].innerHTML=monitorContent[1].innerHTML*USDEUR;
+		} else if(monitorNum==1){
+			var m1=monitorContent[monitorNum].innerHTML;
+			monitorContent[0].innerHTML=m1*USDCNY;
+			monitorContent[2].innerHTML=m1*USDEUR;
+		} else if(monitorNum==2){
+			var m2=monitorContent[monitorNum].innerHTML;
+			monitorContent[1].innerHTML=m2/USDEUR;
+			monitorContent[0].innerHTML=monitorContent[1].innerHTML*USDCNY;
+		}
 	}
 
 
